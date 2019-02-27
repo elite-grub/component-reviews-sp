ecce6b030eeb827783f5a9a5fbdbf4501ded8e93
sparksparker <sehyunspark@gmail.com>
2019-02-27 00:05:31 -0800

    Render user info

--- client/src/components/ReviewsList.jsx
+++ client/src/components/ReviewsList.jsx
@@ -4,9 +4,100 @@ import ReviewsListEntry from './ReviewsListEntry.jsx';
 const ReviewsList = ({reviews}) => {
   return (
     <div>
-      <ReviewsListEntry
-        reviews={reviews}
-      />
+
+      <div
+        className='headerContainer'>
+        <div
+          className='headerTextContainer'>
+          <div
+            className='recommendedReviews'>
+            Recommended Reviews
+          </div>
+          <div
+            className='forCurrentRestaurant'>
+            for Fog Harbor Fish House
+          </div>
+        </div>
+        <div
+          className='searchBarContainer'>
+          <div
+            className='searchBar'>
+            <input
+              type='text'
+              defaultValue='Search within the reviews'
+            />
+          </div>
+          <div
+            className='searchButton'>
+            <button>
+              Go
+            </button>
+          </div>
+        </div>
+        <div
+          className='sortContainer'>
+          <div
+            className='sortBy'>
+            Sort by
+          </div>
+          <div
+            className='sortDropDown'>
+            <select>
+              <option
+                defaultValue='yelp'>
+                Yelp Sort
+              </option>
+              <option
+                value='newest'>
+                Newest First
+              </option>
+              <option
+                value='oldest'>
+                Oldest First
+              </option>
+              <option
+                value='highest'>
+                Highest Rated
+              </option>
+              <option
+                value='lowest'>
+                Lowest Rated
+              </option>
+            </select>
+          </div>
+        </div>
+        <div
+          className='languageContainer'>
+          <div
+            className='language'>
+            Language
+          </div>
+          <div
+            className='languageDropDown'>
+            <select>
+              <option
+                defaultValue='english'>
+                English (5007)
+              </option>
+              <option
+                value='japanese'>
+                Japanese (9)
+              </option>
+              <option
+                value='portugese'>
+                Portugese (6)
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <ReviewsListEntry
+          reviews={reviews}
+        />
+      </div>
+
     </div>
   )
 }
--- client/src/components/ReviewsListEntry.jsx
+++ client/src/components/ReviewsListEntry.jsx
@@ -3,7 +3,127 @@ import React from 'react';
 const ReviewsListEntry = ({reviews}) => {
   return (
     <div>
-      {console.log(reviews)}
+      <ul
+        className='allReviewsContainer'>
+        <li
+          className='emptyDefaultContainer'>
+          <div
+            className='emptyAvatarContainer'>
+            <div
+              className='emptyProfile'>
+              {<img
+                src='https://s3-us-west-1.amazonaws.com/elite-grub-collateral/empty_profile.png'
+              />}
+            </div>
+            <div
+              className='emptyStartReviewContainer'>
+              <div
+                className='emptyStarsContainer'>
+                <div
+                  className='emptyStars'>
+                </div>
+              </div>
+              <div
+                className='startReviews'>
+                Start your review of {''}
+                <strong>Fog Harbor Fish House</strong>
+              </div>
+            </div>
+          </div>
+        </li>
+        {reviews.map(review => (
+          <li
+            className='eachReviewLinkedList'>
+            <div
+              className='eachReviewContainer'>
+              <div
+                className='reviewSidebarContentContainer'>
+                <div
+                  className='profileContainer'>
+                  <div
+                    className='avatarContainer'>
+                    <div
+                      className='avatar'>
+                      {/* avatar */}
+                    </div>
+                  </div>
+                  <div
+                    className='detailsContainer'>
+                    <ul
+                      className='userInfoContainer'>
+                      <li
+                        className='userName'>
+                        Ben D.
+                      </li>
+                      <li
+                        className='locationInfo'>
+                        Southgate, MI
+                      </li>
+                    </ul>
+                    <ul
+                      className='userStatsContainer'>
+                      <li
+                        className='friendCount'>
+                        37 Friends
+                      </li>
+                      <li
+                        className='reviewCount'>
+                        131 reviews
+                      </li>
+                      <li
+                        className='photoCount'>
+                        29 Photos
+                      </li>
+                      <li
+                        className='isElite'>
+                        Elite '19'
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <ul
+                  className='profileLinksContainer'>
+                  <li
+                    className='shareReviewLink'>
+                    Share review
+                  </li>
+                  <li
+                    className='embedReviewLink'>
+                    Embed review
+                  </li>
+                  <li
+                    className='complimentLink'>
+                    Compliment
+                  </li>
+                  <li
+                    className='sendMessageLink'>
+                    Send message
+                  </li>
+                  <li
+                    className='followUserLink'>
+                    Follow Ben D.
+                  </li>
+                </ul>
+              </div>
+              <div
+                className='reviewContentContainer'>
+                <div
+                  className='reviewStarsContainer'>
+                  <div
+                    className='reviewStars'>
+                    *****
+                  </div>
+                  <div
+                    className='reviewDate'>
+                    1/25/2019
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
