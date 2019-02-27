10883a42c0df556f07ee0d2f0ea4933be509b37c
sparksparker <sehyunspark@gmail.com>
2019-02-27 03:28:53 -0800

    Test init files

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,3 +1,10 @@
+import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../client/src/components/App.jsx';
 
+describe('<App />', () => {
+  it('renders one <ReviewList /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(ReviewList).not.have.lengthOf(1));
+  });
+});
